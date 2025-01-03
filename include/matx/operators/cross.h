8900ee361c3bc792c67149682a7c49de91cf13da
Michael Mullen <michael@adialante.com>
2025-01-03 12:56:13 -0600

    saving progress

--- include/matx/operators/cross.h
+++ include/matx/operators/cross.h
@@ -97,7 +97,7 @@ namespace matx
           // cuda::std::array idx{indices...};
           
           cuda::std::array idxA = get_array_from_tuple(pp_get_range<out_rank-OpA::Rank(),out_rank>(indices...));
-          cuda::std::array idxB {pp_get_range<out_rank-OpB::Rank(),out_rank>(indices...)};
+          cuda::std::array idxB = get_array_from_tuple(pp_get_range<out_rank-OpB::Rank(),out_rank>(indices...));
 
           //create references to individual slices for ease of notation
           cuda::std::array idxA0 = idxA;
